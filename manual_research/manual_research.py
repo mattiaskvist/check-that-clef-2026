3069a3e4f80db24faa5aa0c1e2fb7c7060223f5c
dsuzener <141687334+dsuzener@users.noreply.github.com>
2026-04-08 16:44:01 +0200

    exp 6: discard remove abstract

--- manual_research/manual_research.py
+++ manual_research/manual_research.py
@@ -9,7 +9,7 @@ import string
 
 
 # Config
-EXPERIMENT_COUNT = 5
+EXPERIMENT_COUNT = 6
 LANG = "en"
 PERCENT = 5
 TOP_K = 50
@@ -50,7 +50,7 @@ def build_article(row):
     authors = row["authors"]
     venue = row["venue"]
 
-    return title * 3 + " " + authors + " " + venue * 2 + " " + abstract
+    return title * 3 + " " + authors + " " + venue * 2
 
 
 # Load datasets
