1f543c128ec5a4b16e55625fc6838a59114bde1c
dsuzener <141687334+dsuzener@users.noreply.github.com>
2026-04-08 16:27:07 +0200

    exp 4: discard title 4x

--- manual_research/manual_research.py
+++ manual_research/manual_research.py
@@ -50,7 +50,7 @@ def build_article(row):
     authors = row["authors"]
     venue = row["venue"]
 
-    return title * 3 + " " + authors + " " + venue * 2 + " " + abstract
+    return title * 4 + " " + authors + " " + venue * 2 + " " + abstract
 
 
 # Load datasets
